7d67e18b4474a64be4501c09ec48c29bf87ff969
Diggo <rdiggosilva@gmail.com>
2024-07-08 20:32:35 -0300

    Starting screen

---
+++ Login/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Login
+//
+//  Created by Diggo Silva on 08/07/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+}
